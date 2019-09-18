5589b4ca059a8e297ca5900e4c816a86f427c2c1
spark <sparkdev@163.com>
2019-09-18 14:55:07 +0800

    add daemon demo

---
+++ daemondemo.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+
+static void demo_daemon()
+{
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    //signal(SIGCHLD, SIG_IGN);
+    //signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+
+    /* Open the log file */
+    openlog ("daemondemo", LOG_PID, LOG_DAEMON);
+}
+
+int main()
+{
+    int count = 0;
+    demo_daemon();
+
+    while (1)
+    {
+        //TODO: Insert daemon code here.
+        count ++;
+        syslog (LOG_NOTICE, "Daemon demo is running, number: %d", count);
+        sleep (5);
+        if(count > 5)
+        {
+            break;
+        }
+    }
+
+    syslog (LOG_NOTICE, "Daemon demo terminated.");
+    closelog();
+
+    return EXIT_SUCCESS;
+}
